88f0e949ba56306975bc872270416ac4e5992da2
Mendel Kurland <mendel@kurland.me>
2012-11-19 00:18:13 -0600

    CSS Updates and Search Box Created

---
+++ TemplateFiles/RightBar.php
@@ -0,0 +1,1 @@
+<div class="SearchBox"><form method="get"><input id="q" name="q" value="Search..." class="SearchBar"></form></div>
--- index.php
+++ index.php
@@ -31,8 +31,10 @@ loadDatafromRanch ( array (
 
 <body>
 	<div class="AllPanes">
+	<div class="LoginPane"><div class="MenuPadding">Login</div></div>
 	<div class="MenuPane"><div class="MenuPadding">This is where the menu goes</div></div>
-		<div class="LeftPane"><div class="LeftPanePadding"><?php DisplayWiki($GLOBALS['wiki']); ?></div></div>
+	
+	<div class="LeftPane"><div class="LeftPanePadding">Related to <?=$q?><hr><?php DisplayWiki($GLOBALS['wiki']); ?></div></div>
 		<div class="MiddlePane">
 			<div class="SearchName">
 				<h1 class="SearchTitle"><?=strtoupper($q)?></h1>
